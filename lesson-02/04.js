5aee55d6c797ca9f3bfe5abe647bbf2bf1887ce2
Ivan1990Nik <pelevin.vanyusha90@mail.ru>
2024-09-16 20:17:15 +0500

    add_lesson-02-4

--- lesson-02/04.js
+++ lesson-02/04.js
@@ -1,10 +1,10 @@
 // code
 
-const giveMeRightNumbers = (value1, value2, maximum) => {
+const giveMeRight1Numbers = (value1, value2, maximum) => {
   if (value1 === value2 && value2 <= maximum) {
       console.log('Success!');
   } else {
       console.log('Something wrong!');
   }
 }
-giveMeRightNumbers(201, 100, 200)
+giveMeRight1Numbers(201, 100, 200)
